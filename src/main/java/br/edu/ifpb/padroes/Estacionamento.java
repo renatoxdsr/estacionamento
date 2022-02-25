cb4886056362c4e2274cb555954b2644879058e7
Renato Xavier <59875607+renatoxdsr@users.noreply.github.com>
2022-02-25 20:19:19 -0300

    Update Estacionamento.java

--- src/main/java/br/edu/ifpb/padroes/Estacionamento.java
+++ src/main/java/br/edu/ifpb/padroes/Estacionamento.java
@@ -18,20 +18,42 @@ public class Estacionamento {
     public static BigDecimal VALOR_DIARIA = new BigDecimal("26.00");
     public static BigDecimal VALOR_MENSALIDADE = new BigDecimal("300.00");
 
+
     public BigDecimal obterTotalAPagar() {
         assert(entrada != null && saida != null && veiculo != null);
+
         long periodoHoras = Duration.between(entrada, saida).toHours();
         long periodoDias = Duration.between(entrada, saida).toDays();
 
+        BigDecimal valorHoras = calcularHoras(periodoHoras);
+        BigDecimal valorDiarias = calcularDiaria(periodoHoras, periodoDias);
+        BigDecimal valorMensalidade = calcularMensalidade(periodoDias);
+        BigDecimal total = valorHoras.add(valorDiarias).add(valorMensalidade);
+
+        return total;
+    }
+
+    public BigDecimal calcularHoras(long periodoHoras){
         BigDecimal valor = new BigDecimal(0);
         if (periodoHoras < 12) {
-            valor = VALOR_HORA.multiply(new BigDecimal(periodoHoras));
-        } else if (periodoHoras > 12 && periodoDias < 15) {
-            valor = VALOR_DIARIA.multiply(new BigDecimal(periodoDias));
-        } else if (periodoDias > 15) {
+             valor = VALOR_HORA.multiply(new BigDecimal(periodoHoras));
+        }
+        return valor;
+    }
+
+    public BigDecimal calcularDiaria(long periodoHoras, long periodoDias){
+        BigDecimal valor = new BigDecimal(0);
+        if (periodoHoras > 12 && periodoDias < 15) {
+           valor = VALOR_DIARIA.multiply(new BigDecimal(periodoDias));
+        }
+        return valor;
+    }
+
+    public BigDecimal calcularMensalidade(long periodoDias){
+        BigDecimal valor = new BigDecimal(0);
+        if (periodoDias > 15) {
             valor = VALOR_MENSALIDADE;
         }
-
         return valor;
     }
 
